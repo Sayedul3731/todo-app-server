cdcde5d49687b9af83ada7457954e54e817aaef8
MD SAYEDUL HAQUE <125432652+Sayedul3731@users.noreply.github.com>
2024-02-11 21:46:02 +0600

    get new todo data from frontend and the data save to database

--- index.js
+++ index.js
@@ -26,7 +26,13 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+        const todosCollection = client.db('todoDB').collection('todos')
 
+        app.post('/todos', async(req, res) => {
+            const newInfo = req.body;
+            const result = await todosCollection.insertOne(newInfo);
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
